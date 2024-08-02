ed8f8cb5b402973d0b89f81b5824ceffa5469cdd
Dmitry Lorents <zmey.task@gmail.com>
2024-08-02 09:26:46 +0300

    Delete unused code

--- TeslaApp/App/TeslaAppApp.swift
+++ TeslaApp/App/TeslaAppApp.swift
@@ -13,8 +13,6 @@ struct TeslaAppApp: App {
         WindowGroup {
             NavigationView {
                 SplashScreenView()
-//                ChargeView()
-//                MainTabView()
             }
         }
     }
